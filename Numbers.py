842efc96044f3da01284a47e198a2170841d34d2
JuanDavidOrduna <31625405+JuanDavidOrduna@users.noreply.github.com>
2017-10-03 16:26:27 -0500

    First Files Comitting
    First Files Commit

---
+++ Numbers.py
@@ -0,0 +1,47 @@
+"""
+Numbers.py
+Problem: test number operations
+Target Users: Me and that's it
+Target System: Windows
+Interface: Command-line
+Functional Requirements: Test number creations
+Testing: Simple run test
+Maintainer: maintainer@website.com
+"""
+__version__ = 0.1
+
+muscle = 2 + 3
+brainz = 7 - 3
+speed = 5 * -4
+strangeness = muscle + brainz * speed
+strangeness
+
+
+charm = (muscle + brainz) * speed
+charm
+
+print(13 / 5)
+
+print(13.75 / 4.25)
+
+print(13 // 5)
+
+print(13 % 5)
+
+str(strangeness)
+float(strangeness)
+
+#Octal number practice
+octal_number = 0o12
+hexadecimal_number = 0xFC6
+print(octal_number)
+print(hexadecimal_number)
+
+>>> permissions = 0o755
+>>> gold = 0xFFCC00
+
+>>> x = 0xaa + 0x33
+>>> hex(x)
+
+>>> o = 0o77 + 0o33
+>>> oct(o)
