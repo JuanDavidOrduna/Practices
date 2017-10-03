4125807ed68af1bc5e9b4645c17cd77e58dcf27c
JuanDavidOrduna <31625405+JuanDavidOrduna@users.noreply.github.com>
2017-10-03 16:37:54 -0500

    Second Upload
    Additional exercises

---
+++ Conditionals.py
@@ -0,0 +1,42 @@
+ -2 < 5
+
+ 49 > 37
+
+ 7.65 != 6.0
+
+ -5 <= -2
+
+ 7 < -7
+
+ 23.5 > 37.75
+
+ -5 >= 5
+
+ 3.2 != 3.2
+
+
+"alpha" < "omega"
+
+"case" == "Case"
+
+"net" < "network"
+
+"Same" != "Same"
+
+
+ len("alpha") < len("omega")
+
+ len("case") == len("Case")
+
+ len("net") < len("network")
+
+ len("Same") != len("Same")
+
+char1, char2, char3 = 'cat'
+char1
+
+char2
+
+char3
+
+height, length = length, height#error
